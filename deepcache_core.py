c6795beb45be3178a0e87c5ace600828e14dab69
Logan Booker <loganbooker@gmail.com>
2025-05-18 11:04:29 +1000

    Version for sd-forge-classic, with new features a fixes

--- deepcache_core.py
+++ deepcache_core.py
@@ -4,61 +4,78 @@ https://arxiv.org/abs/2312.00858
 start_step, end_step: apply this method when the timestep is between start_step and end_step
 cache_interval: interval of caching (1 means no caching)
 cache_depth: depth of caching
-pow_curve: increase the cache interval over time, clamped and scale to the start and end steps (0 means disabled)
 force_step: after this step, ignore the interval and always use the cache
 '''
 
 import torch
-from backend.nn.unet import timestep_embedding, apply_control
+from ldm_patched.ldm.modules.diffusionmodules.openaimodel import forward_timestep_embed, timestep_embedding, apply_control
 import types
+import sys, os
 
 class DeepCacheStore:
-    def __init__(self, depth, interval, start_step, end_step, force_step, pow_curve):
-        self.step_shape = None
+    def __init__(self, depth, interval, start_step, end_step, force_step, smart_cache):
+        self.last_timestep = -1
         self.current_timestep = -1
 
-        # TODO: Cache age... entries older than interval should probably not be reused.
+        self.cache_key = None
+        self.sub_cache = {}
+        self.cache_sub_key = None
 
-        # Cache is a keyed dictionary based on batch size. Also store step and timestep
-        # information per key. Different batch sizes should not affect each other. For example,
-        # a PAG step with a single batch, versus a regular batch.
-        self.cache = {}
+        self.step = 0
+        self.cache_activated_step = -1
 
         self.depth = depth
         self.interval = interval
         self.start_step = start_step
         self.end_step = end_step
         self.force_step = force_step
-        self.pow_curve = pow_curve
+        self.smart_cache = smart_cache
 
     def __get_cache_interval(self):
-        if self.pow_curve <= 0:
-            return self.interval
-        
-        step_mod = self.current_timestep - self.start_step
-        step_mod_end = 1000 - self.start_step
-        step_frac = (step_mod / step_mod_end) ** self.pow_curve
+        return self.interval
 
-        return 1 + round((self.interval - 1) * step_frac)
-
-    def __have_cache_for_key(self):
-        return self.step_shape in self.cache
+    def __have_cache(self):
+        return self.sub_cache.get(self.cache_sub_key, None) is not None
 
     def __is_cache_active(self):
         return self.start_step <= self.current_timestep <= self.end_step
 
     def __is_caching_step(self):
-        if self.force_step > 0 and self.current_timestep >= self.force_step and self.__have_cache_for_key():
+        if self.force_step > 0 and self.current_timestep >= self.force_step and self.__have_cache():
             return False
 
-        cache = self.cache.get(self.step_shape)
         interval = self.__get_cache_interval()
+        return (self.current_timestep != self.cache_activated_step and self.step % interval == 0) or self.sub_cache.get(self.cache_sub_key, None) is None
 
-        return cache is None or cache["step"] % interval == 0
+    def __clear_caches(self):
+        for k in list(self.sub_cache.keys()):
+            v = self.sub_cache[k]
+            if v is not None:
+                del self.sub_cache[k]
+
+        self.sub_cache = {}
+        self.cache_key = None
+        self.cache_sub_key = None
+        self.step = 0
 
     def setup(self, value, step):
-        self.step_shape = value.shape[0]
+        key, sub_key = DeepCache.fast_context_after_common_call(debug=False)
+        sub_key = f"{sub_key}-{value.shape[0]}"
+        
+        # When smart caching is disabled, clear the cache the moment we believe an extension has
+        # made a sampling call or batch size has changed.
+        if (self.cache_key is None or self.cache_key != key) or \
+           (not self.smart_cache and self.cache_sub_key is not None and self.cache_sub_key != sub_key):
+            self.__clear_caches()
+
+        self.cache_key = key
+        self.cache_sub_key = sub_key
+        
+        self.last_timestep = self.current_timestep
         self.current_timestep = step
+
+        if self.__is_cache_active() and self.cache_activated_step < 0:
+            self.cache_activated_step = step
 
     def skip_unet_block(self, block_id, i=0, block_count=0):
         if not self.__is_cache_active():
@@ -84,30 +101,53 @@ class DeepCacheStore:
         is_cache_step = self.__is_caching_step()
 
         # active, cache_step
-        return (is_active and is_cache_level, is_cache_step)
+        return (is_active, is_cache_level, is_cache_step)
     
     def get_cache(self):
-        cache = self.cache.get(self.step_shape)
-        return None if cache is None else cache["value"]
+        return self.sub_cache.get(self.cache_sub_key, None)
     
     def set_cache(self, value):
-        self.cache[self.step_shape] = { "value": value, "step": 0, "timestep": -1 }
+        cached_value = self.sub_cache.get(self.cache_sub_key, None)
+        if cached_value is not None:
+            del cached_value
+        self.sub_cache[self.cache_sub_key] = value
 
     def update_step(self):
         if self.__is_cache_active():
-            if self.__have_cache_for_key():
-                key = self.step_shape
-                timestep = self.cache[key]["timestep"]
-
-                # Don't increment step if the timestep hasn't changed.
-                if self.current_timestep > timestep:
-                    self.cache[key]["step"] += 1
-                    self.cache[key]["timestep"] = self.current_timestep
-        else:
-            self.cache = {}
+            if self.current_timestep != self.last_timestep:
+                self.step += 1
 
 class DeepCache:
     ORIGINAL_FORWARD_ATTRIBUTE = "_deepcache_original_forward"
+    enabled = True
+
+    @staticmethod
+    def fast_context_after_common_call(max_depth=100, debug=False):
+        wrapper_funcs = {"_wrapped_call_impl", "_call_impl", "decorate_context", "<lambda>"}
+        target_suffix, target_func = os.path.normpath("modules/processing.py"), "process_images"
+        sample_suffix, sample_func = os.path.normpath("modules/samplers.py"), "sampling_function"
+
+        last_func = sub_func = None
+        found_common = False
+
+        for i in range(max_depth):
+            try:
+                frame = sys._getframe(i)
+            except ValueError:
+                break
+            
+            filename, func = frame.f_code.co_filename, frame.f_code.co_name
+            if func in wrapper_funcs:
+                continue
+            if not found_common:
+                if filename.endswith(target_suffix) and func == target_func:
+                    found_common = True
+                elif filename.endswith(sample_suffix) and func == sample_func:
+                    sub_func = last_func
+            else:
+                return (f"{target_suffix}:{func}", sub_func)
+            last_func = func
+        return None
 
     def try_remove(self, model):
         # Remove the patched method, and use the original stored forward method.
@@ -115,7 +155,7 @@ class DeepCache:
         if hasattr(model.model.diffusion_model.forward, DeepCache.ORIGINAL_FORWARD_ATTRIBUTE):
             model.model.diffusion_model.forward = getattr(model.model.diffusion_model.forward, DeepCache.ORIGINAL_FORWARD_ATTRIBUTE)
 
-    def apply(self, model, arg_cache_interval, cache_depth, start_step, end_step, force_step, pow_curve):
+    def apply(self, model, arg_cache_interval, cache_depth, start_step, end_step, force_step, smart_cache):
 
         # Capture the original forward method for the decorator. Once the method has been patched, this will
         # be the patched method, but that's OK as we never repatch and won't overwrite the method.
@@ -126,7 +166,22 @@ class DeepCache:
             return func
 
         @deepcache_patched_decorator
+        @torch.inference_mode()
         def forward_deepcache_patched(self, x, timesteps=None, context=None, y=None, control=None, transformer_options={}, **kwargs):
+            
+            # Use original forward pass when disabled.
+            if not DeepCache.enabled:
+                return original_forward(x, timesteps, context, y, control, transformer_options, **kwargs)
+
+            """
+            Apply the model to an input batch.
+            :param x: an [N x C x ...] Tensor of inputs.
+            :param timesteps: a 1-D batch of timesteps.
+            :param context: conditioning plugged in via crossattn
+            :param y: an [N] Tensor of labels, if class-conditional.
+            :return: an [N x C x ...] Tensor of outputs.
+            """
+
             # Fetch the store from the model.
             cache_store = self._deepcache_store
 
@@ -134,79 +189,102 @@ class DeepCache:
             transformer_options["transformer_index"] = 0
             transformer_patches = transformer_options.get("patches", {})
             block_modifiers = transformer_options.get("block_modifiers", [])
-            assert (y is not None) == (self.num_classes is not None)
+
+            # This code must be kept in sync.
+            # https://github.com/lllyasviel/stable-diffusion-webui-forge/blob/29be1da7cf2b5dccfc70fbdd33eb35c56a31ffb7/ldm_patched/ldm/modules/diffusionmodules/openaimodel.py#L831
+            assert (y is not None) == (
+                self.num_classes is not None
+            ), "must specify y if and only if the model is class-conditional"
             hs = []
             t_emb = timestep_embedding(timesteps, self.model_channels, repeat_only=False).to(x.dtype)
             emb = self.time_embed(t_emb)
+
             if self.num_classes is not None:
                 assert y.shape[0] == x.shape[0]
                 emb = emb + self.label_emb(y)
+
             h = x
 
+            # DeepCache
             cache_store.setup(h, 1000 - timesteps[0].item())
-
+            
             for id, module in enumerate(self.input_blocks):
                 # DeepCache
                 if cache_store.skip_unet_block("input", id):
                     break
 
                 transformer_options["block"] = ("input", id)
+
                 for block_modifier in block_modifiers:
                     h = block_modifier(h, 'before', transformer_options)
-                h = module(h, emb, context, transformer_options)
+
+                h = forward_timestep_embed(module, h, emb, context, transformer_options)
                 h = apply_control(h, control, 'input')
+
                 for block_modifier in block_modifiers:
                     h = block_modifier(h, 'after', transformer_options)
+
                 if "input_block_patch" in transformer_patches:
                     patch = transformer_patches["input_block_patch"]
                     for p in patch:
                         h = p(h, transformer_options)
+
                 hs.append(h)
                 if "input_block_patch_after_skip" in transformer_patches:
                     patch = transformer_patches["input_block_patch_after_skip"]
                     for p in patch:
                         h = p(h, transformer_options)
-            
+
+            # DeepCache
             if not cache_store.skip_unet_block("middle"):
                 transformer_options["block"] = ("middle", 0)
+
                 for block_modifier in block_modifiers:
                     h = block_modifier(h, 'before', transformer_options)
-                h = self.middle_block(h, emb, context, transformer_options)
+
+                h = forward_timestep_embed(self.middle_block, h, emb, context, transformer_options)
                 h = apply_control(h, control, 'middle')
+
                 for block_modifier in block_modifiers:
                     h = block_modifier(h, 'after', transformer_options)
 
+            block_count = len(self.output_blocks)
             for id, module in enumerate(self.output_blocks):
                 # DeepCache
-                if cache_store.skip_unet_block("output", id, len(self.output_blocks)):
+                if cache_store.skip_unet_block("output", id, block_count):
                     continue
 
                 # DeepCache - START
-                is_active, cache_step = cache_store.cache_unet_block(id, len(self.output_blocks))
+                is_active, is_level, cache_step = cache_store.cache_unet_block(id, block_count)
 
-                if is_active:
+                if is_level:
                     if cache_step:
                         cache_store.set_cache(h)
-                    else:
+                    elif is_active:
                         h = cache_store.get_cache()
                 # DeepCache - END
 
                 transformer_options["block"] = ("output", id)
                 hsp = hs.pop()
                 hsp = apply_control(hsp, control, 'output')
+
                 if "output_block_patch" in transformer_patches:
                     patch = transformer_patches["output_block_patch"]
                     for p in patch:
                         h, hsp = p(h, hsp, transformer_options)
+
                 h = torch.cat([h, hsp], dim=1)
                 del hsp
                 if len(hs) > 0:
                     output_shape = hs[-1].shape
                 else:
                     output_shape = None
+
                 for block_modifier in block_modifiers:
                     h = block_modifier(h, 'before', transformer_options)
-                h = module(h, emb, context, transformer_options, output_shape)
+
+                h = forward_timestep_embed(module, h, emb, context, transformer_options, output_shape)
+
                 for block_modifier in block_modifiers:
                     h = block_modifier(h, 'after', transformer_options)
 
@@ -214,16 +292,18 @@ class DeepCache:
             cache_store.update_step()
 
             transformer_options["block"] = ("last", 0)
+
             for block_modifier in block_modifiers:
                 h = block_modifier(h, 'before', transformer_options)
-            if "group_norm_wrapper" in transformer_options:
-                out_norm, out_rest = self.out[0], self.out[1:]
-                h = transformer_options["group_norm_wrapper"](out_norm, h, transformer_options)
-                h = out_rest(h)
+
+            if self.predict_codebook_ids:
+                h = self.id_predictor(h)
             else:
                 h = self.out(h)
+            
             for block_modifier in block_modifiers:
                 h = block_modifier(h, 'after', transformer_options)
+
             return h.type(x.dtype)
 
         new_model = model.clone()
@@ -232,13 +312,13 @@ class DeepCache:
         # store the original forward pass on our patched method.
         if not hasattr(new_model.model.diffusion_model.forward, DeepCache.ORIGINAL_FORWARD_ATTRIBUTE):
             method_name = new_model.model.diffusion_model.forward.__qualname__
-            if (method_name != 'IntegratedUNet2DConditionModel.forward'):
-                print(f"\n[DeepCache] Warning: Unet forward pass appears to be patched by another extension ({method_name})! DeepCache disabled.")
+            if method_name != 'UNetModel.forward':
+                print(f"[-] DeepCache: \033[91mCaching disabled\033[0m - UNet forward pass already patched by another extension ({method_name}).")
                 return
             
             new_model.model.diffusion_model.forward = types.MethodType(forward_deepcache_patched, new_model.model.diffusion_model)
 
         # Store this run's cache on the model itself.
-        new_model.model.diffusion_model._deepcache_store = DeepCacheStore(cache_depth, arg_cache_interval, start_step, end_step, force_step, pow_curve)
+        new_model.model.diffusion_model._deepcache_store = DeepCacheStore(cache_depth, arg_cache_interval, start_step, end_step, force_step, smart_cache)
 
         return (new_model, )
--- scripts/deepcache.py
+++ scripts/deepcache.py
@@ -8,6 +8,9 @@ deepcache_helper = DeepCache()
 
 class DeepCacheScript(scripts.Script):
     sorting_priority = 0
+    use_with_adetailer = False
+    use_with_img2img = False
+    initialised = False
 
     def title(self):
         return "DeepCache"
@@ -19,17 +22,16 @@ class DeepCacheScript(scripts.Script):
         with gr.Accordion(open=False, label=self.title()):
             with gr.Row(equal_height=True):
                 deepcache_mode = gr.Radio(['None', 'First', 'Hires', 'Both'], label='Enable DeepCache for passes', value='None')
-                pow_curve = gr.Slider(label='Cache interval power curve', minimum=0, maximum=10, step=0.1, value=0)
                 force_step = gr.Slider(label='Force cache after sigma', minimum=0, maximum=80, step=0.01, value=0.1)
 
             gr.HTML(value='<div style="height:8px"></div>')
 
             with gr.Row(equal_height=True):
                 cache_interval = gr.Slider(label='Cache interval', minimum=1, maximum=1000, step=1, value=3)
-                cache_depth = gr.Slider(label='Cache depth', minimum=0, maximum=12, step=1, value=5)
+                cache_depth = gr.Slider(label='Cache depth', minimum=0, maximum=12, step=1, value=4)
                 
             with gr.Row(equal_height=True):
-                start_step = gr.Slider(label='Cache start sigma', minimum=0, maximum=80, step=0.01, value=2)
+                start_step = gr.Slider(label='Cache start sigma', minimum=0, maximum=80, step=0.01, value=1)
                 end_step = gr.Slider(label='Cache end sigma', minimum=0, maximum=1, step=0.01, value=0)
 
             gr.HTML(value='<div style="height:8px"></div>')
@@ -41,28 +43,63 @@ class DeepCacheScript(scripts.Script):
 
                 with gr.Row(equal_height=True):
                     hr_cache_interval = gr.Slider(label='HR Cache interval', minimum=1, maximum=1000, step=1, value=3)
-                    hr_cache_depth = gr.Slider(label='HR Cache depth', minimum=0, maximum=12, step=1, value=5)
+                    hr_cache_depth = gr.Slider(label='HR Cache depth', minimum=0, maximum=12, step=1, value=4)
 
                 with gr.Row(equal_height=True):
-                    hr_start_step = gr.Slider(label='HR Cache start sigma', minimum=0, maximum=80, step=0.01, value=2)
+                    hr_start_step = gr.Slider(label='HR Cache start sigma', minimum=0, maximum=80, step=0.01, value=80)
                     hr_end_step = gr.Slider(label='HR Cache end sigma', minimum=0, maximum=1, step=0.01, value=0)
 
-            with gr.Accordion(open=False, label='Compatibility settings'):
-                apply_to_adetailer_pass = gr.Checkbox(label='Enable for ADetailer pass', value=True)
+                def toggle_hr_controls(use_first):
+                    state = use_first
+                    return (
+                        gr.update(interactive=not state),
+                        gr.update(interactive=not state),
+                        gr.update(interactive=not state),
+                        gr.update(interactive=not state)
+                    )
 
-        return deepcache_mode, cache_interval, cache_depth, start_step, end_step, force_step, hr_cache_interval, hr_cache_depth, hr_start_step, hr_end_step, use_first_pass_settings, pow_curve, apply_to_adetailer_pass
+                use_first_pass_settings.change(
+                    fn=toggle_hr_controls,
+                    inputs=use_first_pass_settings,
+                    outputs=[hr_cache_interval, hr_cache_depth, hr_start_step, hr_end_step]
+                )
+
+            with gr.Accordion(open=False, label='Compatibility settings'):
+                with gr.Row(equal_height=True):
+                    smart_cache = gr.Checkbox(label='Smart sub-caching', value=True, info='Disable if you get errors or strange results with other extensions active.')
+                    apply_to_adetailer_pass = gr.Checkbox(label='Enable for ADetailer pass', value=True)
+                    apply_to_img2img_pass = gr.Checkbox(label='Enable for img2img', value=False)
+
+        return deepcache_mode, cache_interval, cache_depth, start_step, end_step, force_step, \
+            hr_cache_interval, hr_cache_depth, hr_start_step, hr_end_step, \
+            use_first_pass_settings, apply_to_adetailer_pass, apply_to_img2img_pass, \
+            smart_cache
+
+    def after_extra_networks_activate(self, p, enable, *args, **kwargs):
+        is_ad_pass = getattr(p, "_ad_inner", False)
+        p_type = type(p).__name__
+
+        if is_ad_pass and not self.use_with_adetailer:
+            print("[-] DeepCache: \033[91mCaching disabled\033[0m - Adetailer pass detected.")
+            DeepCache.enabled = False
+        elif not is_ad_pass and not self.use_with_img2img and "StableDiffusionProcessingImg2Img" in p_type:
+            print("[-] DeepCache: \033[91mCaching disabled\033[0m - img2img operation detected.")
+            DeepCache.enabled = False
+        else:
+            DeepCache.enabled = True
 
     def process_before_every_sampling(self, p, *script_args, **kwargs):
-        deepcache_mode, cache_interval, cache_depth, start_step, end_step, force_step, hr_cache_interval, hr_cache_depth, hr_start_step, hr_end_step, use_first_pass_settings, pow_curve, apply_to_adetailer_pass = script_args
+        deepcache_mode, cache_interval, cache_depth, start_step, end_step, force_step, \
+            hr_cache_interval, hr_cache_depth, hr_start_step, hr_end_step, \
+            use_first_pass_settings, apply_to_adetailer_pass, apply_to_img2img_pass, \
+            smart_cache = script_args
 
         unet = p.sd_model.forge_objects.unet
+        
+        self.use_with_adetailer = apply_to_adetailer_pass
+        self.use_with_img2img = apply_to_img2img_pass
 
         if (deepcache_mode == 'None') or (deepcache_mode == 'First' and p.is_hr_pass) or (deepcache_mode == 'Hires' and not p.is_hr_pass):
-            deepcache_helper.try_remove(unet)
-            return
-
-        is_ad_pass = getattr(p, "_ad_inner", False)
-        if is_ad_pass and not apply_to_adetailer_pass:
             deepcache_helper.try_remove(unet)
             return
 
@@ -79,25 +116,18 @@ class DeepCacheScript(scripts.Script):
             hr_cache_depth=hr_cache_depth,
             hr_start_step=hr_start_step,
             hr_end_step=hr_end_step,
-            pow_curve=pow_curve
+            smart_cache=smart_cache
         ))
 
-        if p.is_hr_pass: 
-            # For the hires pass, ignore various settings that we can't
-            # apply consistently.
-            pow_curve = 0                
+        if p.is_hr_pass and not use_first_pass_settings:
+            cache_interval, cache_depth = hr_cache_interval, hr_cache_depth
+            start_step, end_step = hr_start_step, hr_end_step
 
-            if not use_first_pass_settings:
-                cache_interval = hr_cache_interval
-                cache_depth = hr_cache_depth
-                start_step = hr_start_step
-                end_step = hr_end_step
+        start_step = 0 if start_step == 0 else 1000 - unet.model.model_sampling.timestep(torch.tensor(start_step)).item()
+        end_step = 1000 if end_step == 0 else 1000 - unet.model.model_sampling.timestep(torch.tensor(end_step)).item()
+        force_step = 0 if force_step == 0 else 1000 - unet.model.model_sampling.timestep(torch.tensor(force_step)).item()
 
-        start_step = 999999998 if start_step == 0 else 1000 - unet.model.predictor.timestep(torch.tensor(start_step)).item()
-        end_step = 999999999 if end_step == 0 else 1000 - unet.model.predictor.timestep(torch.tensor(end_step)).item()
-        force_step = 0 if force_step == 0 else 1000 - unet.model.predictor.timestep(torch.tensor(force_step)).item()
-
-        unet = deepcache_helper.apply(unet, cache_interval, cache_depth, start_step, end_step, force_step, pow_curve)[0]
+        unet = deepcache_helper.apply(unet, cache_interval, cache_depth, start_step, end_step, force_step, smart_cache)[0]
         p.sd_model.forge_objects.unet = unet
 
         return
